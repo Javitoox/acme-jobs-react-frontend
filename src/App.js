9c3149f5db158a0fdbef04dff391cc76dae5daf0
JAVIER MARTÍNEZ FERNÁNDEZ <javmarfer3@alum.us.es>
2021-05-10 05:21:08 +0200

    Listing shouts

--- src/App.js
+++ src/App.js
@@ -1,38 +1,31 @@
-import logo from "./logo.svg";
 import "./App.css";
 import axios from "axios";
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
 
 class App extends Component {
-
-  action = this.action.bind(this);
-
   state = {
-		data: null
-	}
+    data: null,
+  };
 
-   action(event) {
-    axios
-      .get("http://localhost:8080/Acme-Jobs/anonymous/shout/list")
-      .then((res) => {
-        this.setState({data: res.data[1]})
-      });
+  componentDidMount() {
+    axios.get("http://localhost:8080/Acme-Jobs/api/shout").then((res) => {
+      this.setState({ data: res.data });
+    });
   }
 
-  render(){
+  render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <p onClick={this.action}>First Shout!</p>
-          <p>{this.state.data}</p>
-        </header>
+      <div className="datatable-templating-demo">
+        <div className="card">
+          <DataTable value={this.state.data} header={"Shouts"} footer={""}>
+            <Column field="text" header="Text"></Column>
+          </DataTable>
+        </div>
       </div>
     );
   }
 }
 
-export default App
+export default App;
